c06dcc46a5c79bff02983e8e366911da2dc77f84
Arjen van der Ende <arjen.vanderende@gmail.com>
2014-10-21 10:16:48 +0200

    Add Unity project with parallax scrolling background

---
+++ Assets/Scripts/ParallaxScroller.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxScroller : MonoBehaviour {
+
+	[Range(0, 2)]
+	public float scrollSpeed;
+
+	// Use this for initialization
+	void Start () {	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float offsetX = Mathf.Repeat (Time.time * scrollSpeed, 1);
+		renderer.sharedMaterial.SetTextureOffset("_MainTex", new Vector2 (offsetX, 0));
+	}
+}
