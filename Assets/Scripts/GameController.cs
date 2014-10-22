583b33cb45f7b2e4c277b9fa232c33fea774689e
Arjen van der Ende <arjen.vanderende@gmail.com>
2014-10-22 16:19:10 +0200

    Spawn pipes at random height

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public PipeSpawning pipeSpawning;
+
+	void Start () {
+		StartCoroutine (SpawnPipes());
+	}
+
+	IEnumerator SpawnPipes () {
+		yield return new WaitForSeconds (pipeSpawning.delay);
+
+		while (true) {
+			Vector3 pipeSpawnPosition = pipeSpawning.spawn.position + new Vector3(0, Random.Range(-pipeSpawning.heightRange, pipeSpawning.heightRange), 0);
+			Instantiate(pipeSpawning.pipe, pipeSpawnPosition, Quaternion.identity);
+			yield return new WaitForSeconds (pipeSpawning.delay);
+		}
+	}
+}
+
+[System.Serializable]
+public class PipeSpawning {
+
+	public GameObject pipe;
+	public Transform spawn;
+	public float delay;
+	public int heightRange;
+}
---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+	public float velocity;
+
+	void Start() {
+		gameObject.rigidbody2D.velocity = new Vector2 (velocity, 0f);
+	}
+
+
+
+	 
+}
