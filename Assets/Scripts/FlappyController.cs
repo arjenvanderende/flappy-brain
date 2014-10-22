736fe0dbdf29b3f58e3d4106586a911b31216535
Arjen van der Ende <arjen.vanderende@gmail.com>
2014-10-22 14:53:49 +0200

    Look at flying direction
    Conflicts:
    	Assets/Scripts/FlappyController.cs

--- Assets/Scripts/FlappyController.cs
+++ Assets/Scripts/FlappyController.cs
@@ -3,28 +3,45 @@ using System.Collections;
 
 public class FlappyController : MonoBehaviour {
 
+	public float forwardVelocity;
 	public float impulse;
-	public bool jump;
+
+
+	private bool jump = false;
 
 	void Update () {
 		if (Input.GetButtonDown ("Fire1")) {
 			jump = true;
 		}
 	}
-	
+
+	void Update () {
+		if (Input.GetButton ("Fire1")) {
+			jump = true;
+		}
+
+		Vector2 forward = new Vector2 (forwardVelocity, 0);
+		Vector2 direction = forward + gameObject.rigidbody2D.velocity;
+		float angle = Vector2.Angle (forward, direction);
+		if (direction.y < 0)
+			angle = -angle;
+
+		Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+		gameObject.transform.rotation = rotation;
+	}
+
 	void FixedUpdate () {
 		if (jump) {
-			Jump ();
+			Jump();
 			jump = false;
 		}
 	}
 
-	void Jump() {
-		Vector2 force = -gameObject.rigidbody2D.velocity + new Vector2(0, impulse);
-		gameObject.rigidbody2D.AddForce(force, ForceMode2D.Impulse);
-	}
-
-	public void Flap() {
-		jump = true;
+	void Jump () {
+		// Jumping should always look/work the same, regardless of the current velocity.
+		// So when calculating the force to apply to the object, we need to factor that in.
+		Vector2 force = new Vector2 (0, impulse) - gameObject.rigidbody2D.velocity;
+		Vector2 amplifiedForce = force * gameObject.rigidbody2D.mass;
+		gameObject.rigidbody2D.AddForce(amplifiedForce, ForceMode2D.Impulse);
 	}
 }
